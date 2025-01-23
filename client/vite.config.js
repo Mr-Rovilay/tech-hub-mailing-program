b41b770dca3cd8accab92a8283d840a53ec0a88d
ayodeji akinola <ayodejiakinola007@gmail.com>
2025-01-23 12:31:46 +0100

    s

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,10 +5,6 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // Ensure this is the correct path to your client folder
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
